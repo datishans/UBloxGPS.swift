d083deac1c1bfdb880274156649851eb76cd115d
Hans Bijkerk <datishans@users.noreply.github.com>
2019-04-29 20:03:21 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "UBloxGPS",
     dependencies: [
-        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git",
-                 majorVersion: 1)
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git",
+                 from: "1.0.0")
     ]
 )

16103f4aeb98f5e39f20b475a893330a929069a8
Hans Bijkerk <git@datishans.nl>
2019-04-29 20:18:49 +0200

    no message

--- Package.swift
+++ Package.swift
@@ -9,3 +9,26 @@ let package = Package(
                  from: "1.1.2")
     ]
 )
+
+
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "UBloxGPS",
+    dependencies: [
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git",
+                 from: "1.1.2")
+    ],
+    products: [
+        .library(
+            name: "UBloxGPS",
+            targets: ["UBloxGPS"]),
+    ],
+    targets: [
+        .target(
+            name: "UBloxGPS",
+            path: "Sources"),
+    ]
+)

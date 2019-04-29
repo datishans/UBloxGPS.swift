177a67fb29f490ce3f5bd33ec9c51fd6e9d97bd9
Hans Bijkerk <git@datishans.nl>
2019-04-29 20:19:56 +0200

    no message

--- Package.swift
+++ Package.swift
@@ -4,27 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "UBloxGPS",
-    dependencies: [
-        .package(url: "https://github.com/uraimo/SwiftyGPIO.git",
-                 from: "1.1.2")
-    ]
-)
-
-
-// swift-tools-version:5.0
-
-import PackageDescription
-
-let package = Package(
-    name: "UBloxGPS",
-    dependencies: [
-        .package(url: "https://github.com/uraimo/SwiftyGPIO.git",
-                 from: "1.1.2")
-    ],
     products: [
         .library(
             name: "UBloxGPS",
             targets: ["UBloxGPS"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git",
+                 from: "1.1.2")
     ],
     targets: [
         .target(
